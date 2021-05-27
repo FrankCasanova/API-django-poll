30def2e208e0dad94a91deefc082f45d1eed9b39
Frank Casanova <frankcasanova.info@gmail.com>
2021-05-27 23:25:39 +0200

    improved views with ModelViewSets and router.

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,17 @@
 from django.urls import include, re_path
 from django.urls.conf import path
-from .views import polls_details, polls_list
+from rest_framework.routers import DefaultRouter
+from .apiviews import PollDetail, PollList, ChoiceList, CreateVote
+from .apiviews import PollViewSet
+
+router = DefaultRouter()
 
 urlpatterns = [
-    path('polls/', polls_list, name='polls_list'),
-    path('polls/<int:pk>/', polls_details, name='polls_details')
+    path('polls/', PollList.as_view(), name='polls_list'),
+    path('polls/<int:pk>/', PollDetail.as_view(), name='polls_details'),
+    path('polls/<int:pk>/choices/', ChoiceList.as_view(), name='choice_list'),
+    path('polls/<int:pk>/choices/<choice_pk>/vote/', CreateVote.as_view(), name='create_vote'),
 ]
+
+urlpatterns += router.urls
+
